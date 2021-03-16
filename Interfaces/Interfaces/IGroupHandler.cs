213674acb399161bc862b570472cba2036aea497
TimKuijpers2002 <timkuijpers2002@outlook.com>
2021-03-16 13:11:51 +0100

    Initial commit

---
+++ Interfaces/Interfaces/IGroupHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces.Interfaces
+{
+    public interface IGroupHandler
+    {
+        //CRUD
+    }
+}
---
+++ LOGIC/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LOGIC.Models
+{
+    public class User
+    {
+        public int UserID { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        public User(int userID, string name, string email, string password)
+        {
+            UserID = userID;
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public void DeleteUser(int userID)
+        {
+
+        }
+
+        public void UpdateUser(User user)
+        {
+            //UserDTO _user = new UserDTO;
+
+
+        }
+    }
+}
